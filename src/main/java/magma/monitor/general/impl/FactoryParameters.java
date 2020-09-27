75b79480590154f2caf8e4b76593d8d0051b0809
Hannes Braun <hannesbraun@mail.de>
2020-09-27 22:17:59 +0200

    Add missing copyright headers

--- src/main/java/magma/monitor/general/impl/FactoryParameters.java
+++ src/main/java/magma/monitor/general/impl/FactoryParameters.java
@@ -1,10 +1,7 @@
-/* Copyright 2009 Hochschule Offenburg
- * Klaus Dorer, Mathias Ehret, Stefan Glaser, Thomas Huber,
- * Simon Raffeiner, Srinivasa Ragavan, Thomas Rinklin,
- * Joachim Schilling, Rajit Shahi
+/*******************************************************************************
+ * Copyright 2008 - 2020 Hochschule Offenburg
  *
  * This file is part of magmaOffenburg.
- *
  * magmaOffenburg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,8 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with magmaOffenburg. If not, see <http://www.gnu.org/licenses/>.
- */
-
+ *******************************************************************************/
 package magma.monitor.general.impl;
 
 /**
